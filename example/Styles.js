03a3d83a27cd39da98c3e3c0e6b06e9ba00fa6f8
yuji tsuburaya <nikke.934@gmail.com>
2019-09-09 13:32:46 +0900

    ♻️ example refactor ( create styles object )

---
+++ example/Styles.js
@@ -0,0 +1,50 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  open: {
+    textAlign: 'center',
+    marginTop: 64
+  },
+  modalText: {
+    color: '#FFF'
+  },
+  modalCancelButton: {
+    borderRadius: 32,
+    height: 40,
+    backgroundColor: '#243347',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  bar: {
+    width: 16,
+    borderBottomWidth: 4,
+    borderColor: '#FFFFFF44'
+  },
+  leftBar: {
+    borderRadius: 16,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0
+  },
+  rightBar: {
+    borderRadius: 16,
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0
+  },
+  SemiModal: {
+    paddingVertical: 24,
+    paddingHorizontal: 24,
+    paddingTop: 8,
+    backgroundColor: '#151F2B'
+  },
+  modalInner: {
+    flex: 1,
+    flexDirection: 'row',
+    marginBottom: 24,
+    justifyContent: 'center'
+  }
+});
+
+export default styles;
